e5ca92f4935c39db7819e561e8dba03d5b867b7c
Freddie CME <116894658+freddiekaplan-cme@users.noreply.github.com>
2023-01-27 10:46:25 +0100

    First commit

---
+++ scripts/freddie.js
@@ -0,0 +1,103 @@
+$(function() {
+
+$(".floating-head").on("click", function () {
+	$(".footer").toggleClass("show-footer");
+	$(".footer").slideToggle(700);
+	$(".home-title").toggleClass("show-title");
+	//$(".home-title").slideToggle(1300);
+	/*
+	$(".menu").toggleClass("show-menu");
+	$(".menu").slideToggle(700);
+	*/
+	if ($(".menu").hasClass("show-menu")) {
+		$(".menu").toggleClass("show-menu");
+		$(".level-one").slideUp(300);
+		$(".main-menu").removeClass("active");
+		setTimeout(function(){ 
+			$(".new-menu").remove()
+			}, 300);
+	} else {
+		$(".menu").toggleClass("show-menu");
+		$(".main-menu").removeClass("active");
+	}
+});
+
+$(".spinning").on({
+	click: function () {
+	$(".rotate-star").toggleClass("stop-star-rotation"),
+	$(".start-stop").text("Start");
+	
+	if ($(".rotate-star").hasClass("stop-star-rotation")) {
+		$("start-stop").text("Start")
+	} else {
+		$(".start-stop").text("Stop")
+	}}
+});
+
+$("#projects-menu-item").on("click", function() {
+	if ($(".project-menu").hasClass("active")) {
+		$(".project-menu").slideUp(300);
+		$(".project-menu").removeClass("active");
+	} else {
+		$(".project-menu").slideDown("slow");
+		$(".project-menu").addClass("active");
+	}
+});
+
+$(".sub-menu-item").on("click", function() {
+	if ($(this).hasClass("active-menu")) {
+		$(this).removeClass("active-menu");
+	} else {
+		$(".sub-menu-item").removeClass("active-menu");
+		$(this).addClass("active-menu");
+		$(this).children().find(".sub-sub-menu").slideDown("slow");
+	}
+});
+
+function clearAll() {
+	document.getElementById("name").value = "";
+	document.getElementById("e-mail").value = "";
+	document.getElementById("subject").value = "";
+	document.getElementById("message").value = "";
+	document.getElementById("kap-tcha").value = "";
+	document.getElementById("checkmark").value = "";
+}
+
+$("#clear-button").on({
+	click: function() {
+		clearAll();
+		$("#clear-button").addClass("pressed");
+			setTimeout(function() { 
+				$("#clear-button").removeClass("pressed");
+			}, 1000);
+	}
+});
+
+function clearCheckmark() {
+	$("#checkmark").text("");
+	$("#send-button").removeClass("pressed");
+}
+
+$("#send-button").on({
+	click: function() {
+		if (document.getElementById("kap-tcha").value === "I want to hire you!") {
+			$("#checkmark").text("✅");
+			$("#send-button").addClass("pressed");
+			setTimeout(function() { 
+				clearCheckmark()
+			}, 1000);
+			setTimeout(function() { 
+				$("#form").submit();
+				
+			}, 1001);
+		} else {
+			$("#checkmark").text("❌");
+			$("#send-button").addClass("pressed");
+			setTimeout(function() { 
+				clearCheckmark()
+			}, 1000);
+		}
+	}
+});
+
+});
---
+++ scripts/keebs.js
@@ -0,0 +1,11 @@
+$(function() {
+	$(document).ready(function(){
+		$('.slider-plugin').slick({
+		 // setting-name: setting-value
+		 accessibility: true,
+		 autoplay: true,
+		 responsive: true,
+		 touchMove: true,
+		});
+	  });
+});
